6c090b14ce08d08e7a3adccc2ade5cb7e8809954
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2025-07-30 04:25:15 +0000

    less

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main(){
+	var a, b string
+	var m = map[string]map[string]float64{
+		"rub":{
+			"usd": 0.011,
+			"eur": 0.010,
+		},
+		"usd":{
+			"rub": 91.65,
+			"eur" : 0.92,
+		},
+	}
+	fmt.Scan(&b)
+	fmt.Scan(&a)
+	fmt.Println(m[a][b])
+}
